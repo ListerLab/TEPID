eb0e13aabc5a489e439a762152ba870fcee49740
Tim Stuart <timstuart90@gmail.com>
2014-09-02 14:40:24 +0800

    Updated mapping script
    Now more concise and outputs sorted bedfile.

--- map_discord.sh
+++ map_discord.sh
@@ -2,10 +2,6 @@
 # Usage:
 #   sh map_discord.sh <proc>
 #   where <proc> is number of processors to use
-# Requirements:
-#   samblaster: https://github.com/GregoryFaust/samblaster
-#   bowtie2: http://bowtie-bio.sourceforge.net/bowtie2/index.shtml
-#   samtools: http://samtools.sourceforge.net/samtools.shtml
 # Does the following:
 #   1. Move into directory
 #   2. Maps PE sequencing data using bowtie2
@@ -14,6 +10,7 @@
 #   5. Deletes concordant reads
 #   6. Compresses original fastq files
 #   7. Move into next directory and repeat
+# Outputs a sorted bedfile containing discordant read alignments and a log file
 
 blue='\033[94m'
 NC='\033[0m'
@@ -26,14 +23,21 @@ for directory in ./*; do
             date
             echo -e "${blue}Starting $fname${NC}"
             echo -e '\tMapping'
-            bowtie2 --local -p$1 --fr -q -R5 -N1 -x /dd_stage/userdata/lister/data/genomes/bowtie2_indexes/tair9 -X 2000 -1 "${fname}_1.fastq" -2 "${fname}_2.fastq" -S "${fname}.sam" | tee -a "${fname}.log"
+            bowtie2 --local -p$1 --fr -q -R5 -N1 -x /dd_stage/userdata/lister/data/genomes/bowtie2_indexes/tair9 -X 2000 -1 "${fname}_1.fastq" -2 "${fname}_2.fastq" | samblaster -e -d "${fname}.disc.sam" | samtools view -bS - > "${fname}.disc.bam" | tee -a "${fname}.log"
             echo -e '\tFinished mapping $fname'
-            echo -e '\tConverting output to sorted bam file'
-            samtools view -bS $fname$samname | samtools sort -n - $fname
-            echo -e '\tSplitting discordant reads from bam file'
-            samtools view -h "${fname}.bam" | samblaster -a -e -d "${fname}.disc.bam"
+            echo -e '\tConverting to bedfile'
+            bedtools bamtobed -i "${fname}.disc.bam" -bedpe > "${fname}.disc.bed"
+            sed -i.bak '/chrC/d;/chrM/d;s/chr//g' "${fname}.disc.bed"
+            echo -e '\tSorting bedfile, removing reads mapped to chloroplast or mitochondria'
+            sort -k1,1 -nk2,2 "${fname}.disc.bed" > "${fname}_sorted.disc.bed"
+            echo -e '\tDeleting temp files'
+            rm "${fname}.disc.bed"
+            rm "${fname}.disc.bed.bak"
+            rm "${fname}.disc.sam"
+            rm "${fname}.disc.bam"
             rm "${fname}.sam"
             rm "{$fname}.bam"
+            mv "${fname}_sorted.disc.bed" "${fname}.bed"
             echo -e '\tCompressing fastq files'
             tar cvfz "${fname}.tgz" "${fname}_1.fastq" "${fname}_2.fastq"
             echo -e "${blue}Finished $fname${NC}"
