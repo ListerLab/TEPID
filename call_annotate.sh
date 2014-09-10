77ed6f9b63fde10eac29c53699dc2b97918a6d83
Tim Stuart <timstuart90@gmail.com>
2014-09-10 12:12:21 +0800

    command-line args for repo path

--- call_annotate.sh
+++ call_annotate.sh
@@ -1,9 +1,20 @@
+path=
+
+while getopts p: opt; do
+  case $opt in
+  p)
+      path=$OPTARG
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
+
 for directory in ./*; do
     if [ -d "$directory" ]; then
         cd $directory
         for myfile in $(ls -d *intersections.bed);do
         	fname=(${myfile//_TE_intersections.bed/ })
-            python annotate.py n $fname
+            python "${path}/annotate.py" n $fname
             rm "intersections_ordered_${fname}.bed"
         done
         cd ..
