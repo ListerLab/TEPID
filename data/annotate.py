44c9f990ab85b213b7c4ba23a4d9d5b2748c9d15
Tim Stuart <timstuart90@gmail.com>
2014-09-15 12:06:01 +0800

    annotate.py now goes through all subdirectories
    call_annotate.sh not needed, annotate.py will go through all
    directories and annotate TE insertions.

--- data/annotate.py
+++ data/annotate.py
@@ -1,6 +1,8 @@
 # usage:
-# python annotate.py n <accession_name>
+# run from directory containing all accession subdirectories
+# $ python annotate.py
 
+import os
 from subprocess import call
 from sys import argv
 
@@ -21,25 +23,6 @@ from sys import argv
         - references read names making up annotation
         - can be used to compare between accessions
 """
-
-
-def checkArgs(arg1, arg2):
-    """
-    arg1 is short arg, eg h
-    arg2 is long arg, eg host
-    """
-    args = argv[1:]
-    if arg1 in args:
-        index = args.index(arg1)+1
-        variable = args[index]
-        return variable
-    elif arg2 in args:
-        index = args.index(arg2)+1
-        variable = args[index]
-        return variable
-    else:
-        variable = raw_input("\nEnter {arg2}: ".format(arg2=arg2))
-        return variable
 
 
 def overlap(start1, stop1, start2, stop2):
@@ -260,9 +243,14 @@ def find_next(lines, i, x, chrom, strand, start, stop, te_name):
             if x >= i:
                 return False
 
-
-accession_name = checkArgs('n', 'name')
-reorder('{b}_TE_intersections.bed'.format(b=accession_name), 'intersections_ordered_{b}.bed'.format(b=accession_name))
-call('sort -k1,1 -nk2,2 intersections_ordered_{b}.bed > intersections_sorted_{b}.bed'.format(b=accession_name), shell=True)
-merge('intersections_sorted_{b}.bed'.format(b=accession_name), 'merged_{b}.bed'.format(b=accession_name))
-annotate('merged_{b}.bed'.format(b=accession_name), 'insertions_{b}.bed'.format(b=accession_name), 'id_{b}.fa'.format(b=accession_name))
+for dirs in os.listdir('.'):
+    if os.walk.isdir(dirs) is True:
+        os.chdir(dirs)
+        reorder('{b}_TE_intersections.bed'.format(b=dirs), 'intersections_ordered_{b}.bed'.format(b=dirs))
+        call('sort -k1,1 -nk2,2 intersections_ordered_{b}.bed > intersections_sorted_{b}.bed'.format(b=dirs), shell=True)
+        merge('intersections_sorted_{b}.bed'.format(b=dirs), 'merged_{b}.bed'.format(b=dirs))
+        annotate('merged_{b}.bed'.format(b=dirs), 'insertions_{b}.bed'.format(b=dirs), 'id_{b}.fa'.format(b=dirs))
+        call('rm intersections_ordered_{b}'.format(b=dirs))
+        os.chdir('..')
+    else:
+        pass
