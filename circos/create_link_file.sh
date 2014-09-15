e427f8fc6a1006f4510fd8fe8a30ad8105423aa9
Tim Stuart <timstuart90@gmail.com>
2014-09-15 17:26:14 +0800

    Bug fix, added copy to circos folder

--- circos/create_link_file.sh
+++ circos/create_link_file.sh
@@ -1,10 +1,28 @@
+# usage:
+# sh create_link_file.sh -p <path/to/circos/data/dir>
+
+path=
+
+while getopts p: opt; do
+  case $opt in
+  p)
+      path=$OPTARG
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+
 for directory in ./*; do
     if [ -d "$directory" ]; then
         cd $directory
         for myfile in $(ls -d insertions*);do
-        	sed 1d "insertions_${directory}.bed" > headerless
-			awk 'BEGIN {FS=OFS="\t"} {print "chr"$1,$2,$3,"chr"$7,$8,$9}' "insertions_${directory}.bed" > "circos_${directory}.txt"
+        	fname=(${myfile//insertions_/ })
+        	fname=(${fname//.bed/ })
+        	sed 1d "insertions_${fname}.bed" > headerless
+			awk 'BEGIN {FS=OFS="\t"} {print "chr"$1,$2,$3,"chr"$7,$8,$9}' headerless > "circos_${fname}.txt"
 			rm headerless
+			cp "circos_${fname}.txt" $path
         done
         cd ..
     fi
