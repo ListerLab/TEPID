ddc24e27c63b9d8d62d89e2c0349bb31611765fa
Tim Stuart <timstuart90@gmail.com>
2014-09-17 11:11:57 +0800

    bug fix

--- data/merge_insertions.py
+++ data/merge_insertions.py
@@ -19,9 +19,9 @@ def create_master_dict(master, accession_name):
             if line[0] == 'ins_chr':
                 pass
             else:
-                master_insertions[line[5]] = {'ins_chrom': line[0], 'ins_start': line[1], 'ins_end': line[2],
-                                              'ins_strand': line[3], 'agi': line[4], 'ref_chrom': line[6],
-                                              'ref_start': line[7], 'ref_end': line[8], "ref_strand": line[9], 'accessions': [accession_name]}
+                master_insertions[line[5]] = {'ins_chrom': int(line[0]), 'ins_start': int(line[1]), 'ins_end': int(line[2]),
+                                              'ins_strand': line[3], 'agi': line[4], 'ref_chrom': int(line[6]),
+                                              'ref_start': int(line[7]), 'ref_end': int(line[8]), "ref_strand": line[9], 'accessions': [accession_name]}
         return master_insertions
 
 
@@ -33,17 +33,18 @@ def merge_insertions(master_dict, ins_file, accession_name):
             if ins_chrom == 'ins_chr':
                 pass  # header
             else:
-                ins_start = line[1]
-                ins_end = line[2]
+                ins_chrom = int(ins_chrom)
+                ins_start = int(line[1])
+                ins_end = int(line[2])
                 agi = line[4]
                 ident = line[5]
-                ref_chrom = line[6]
-                ref_start = line[7]
-                ref_end = line[8]
+                ref_chrom = int(line[6])
+                ref_start = int(line[7])
+                ref_end = int(line[8])
                 ref_strand = line[9]
-                for key, value in master_dict.items():
+                for key, value in master_dict.iteritems():
                     if value['ins_chrom'] == ins_chrom and value['agi'] == agi and ref_chrom == value['ref_chrom'] and ref_start == value['ref_start']:
-                        if overlap(int(value['ins_start']), int(value['ins_end']), int(ins_start), int(ins_end)) is True:
+                        if overlap(value['ins_start'], value['ins_end'], ins_start, ins_end) is True:
                             # need to do something with unique id
                             value['accessions'].append(accession_name)
                             # value['ident'].append(ident)
@@ -57,9 +58,9 @@ def merge_insertions(master_dict, ins_file, accession_name):
                         else:
                             # still need to add insertion, but as new entry (not shared by accessions processed so far)
                             # this is where having a unique id is essential
-                            master_insertions[ident] = {'ins_chrom': line[0], 'ins_start': line[1], 'ins_end': line[2],
-                                                        'ins_strand': line[3], 'agi': line[4], 'ref_chrom': line[6],
-                                                        'ref_start': line[7], 'ref_end': line[8], "ref_strand": line[9], 'accessions': [accession_name]}
+                            master_insertions[ident] = {'ins_chrom': int(line[0]), 'ins_start': int(line[1]), 'ins_end': int(line[2]),
+                                                        'ins_strand': line[3], 'agi': line[4], 'ref_chrom': int(line[6]),
+                                                        'ref_start': int(line[7]), 'ref_end': int(line[8]), "ref_strand": line[9], 'accessions': [accession_name]}
                     else:
                         pass
 
@@ -85,7 +86,7 @@ for dirs in os.listdir('.'):
 # save master insertions dict to csv file
 with open('insertions.bed', 'w+') as outfile:
     # outfile.write("ins_chr\tins_start\tins_end\tins_strand\tagi\tref_chr\tref_start\tref_end\tref_strand\tID\taccessions\n")
-    for key, value in master_insertions.items():
+    for key, value in master_insertions.iteritems():
         accessions = set(value['accessions'])  # removes duplicates
         outfile.write("""{ins_chrom}\t{ins_start}\t{ins_end}\t{ins_strand}\t{agi}\t{ref_chrom}\t{ref_start}\t{ref_end}\t{ref_strand}\t{id}\t{accessions}\n""".format(ins_chrom=value['ins_chrom'],
                                                                                                                                                                      ins_start=value['ins_start'],
