fb80a9600032c51c12c151e6fcff2b5603c0adc0
Tim Stuart <timstuart90@gmail.com>
2014-10-30 16:16:58 +0800

    Added function to create list of sample names from file

--- Code/inverse_del.py
+++ Code/inverse_del.py
@@ -1,11 +1,15 @@
+# usage:
+# python inverse_del.py <names> <merged_deletions> <outfile>
+# requires list of all sample names
 
-def filter_del(inf, master):
+
+def filter_del(inf, master, outf):
     """
     Take bedfile containing all TE deletions and create
     polymorphic TE file with coordinates of TE and
     list of accessions that contain the TE
     """
-    with open(inf, 'r') as infile:
+    with open(inf, 'r') as infile, open(outf, 'w+') as outfile:
         for line in infile:
             line = line.rsplit()
             accessions = line[6]
@@ -19,5 +23,17 @@ def filter_del(inf, master):
                 else:
                     pass
             info = '\t'.join(coords) + '\t' + ','.join(ins) + '\n'
-            print info
+            outfile.write(info)
 
+
+def create_names_list(inf):
+    names = []
+    with open(inf, 'r') as infile:
+        for line in infile:
+            names.append(line)
+    return names
+
+if __name__ == "__main__":
+    import sys
+    master = create_names_list(sys.argv[1])
+    filter_del(sys.argv[2], master, sys.argv[3])
