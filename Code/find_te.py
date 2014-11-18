82928769c53f4dd57d9a3f386a5d0b926546b7ef
timoast <timstuart90@gmail.com>
2014-11-18 10:59:31 +0800

    pybedtools pairtobed and sort functions

--- Code/find_te.py
+++ Code/find_te.py
@@ -4,39 +4,63 @@
 
 import pybedtools
 import locate  # best way to do this?
+import os
 
 # need to get input bam file
 # assume input is a sorted bam file
 # - disc_mapped
+# - all_mapped
 # - split_mapped
 # - te_bed
 
 # create bedtool objects, convert to bed, sort
 # need to make object for TE annotation as well
-disc_bam = pybedtools.BedTool(disc_mapped)
-mn, std = locate.calc_mean(disc_bam.head(20000))  # estimate lib size
+bam = pybedtools.BedTool(all_mapped)
+mn, std = locate.calc_mean(bam.head(20000))  # estimate lib size
 
-disc = disc_bam.bam_to_bed(bedpe=True, mate1=True).sort()
-split = pybedtools.BedTool(split_mapped).bam_to_bed().sort()
+disc = pybedtools.BedTool(disc_mapped).bam_to_bed(bedpe=True, mate1=True).sort()
+split = pybedtools.BedTool(split_mapped).bam_to_bed().sort().saveas('split.temp')
 te = pybedtools.BedTool(te_bed).sort()
 
 # remove reads mapped to mito, chlr genomes. May not be needed..
 
 # Convert split bed to bedpe format (already have python script)
-split_bedpe = locate.convert_split_pairbed(split_bed)  # script needs to be edited
+# can make a list of strings and create bedtool
+# append to same bedtool by using:
+# test = pybedtools.BedTool('string', from_string=True).cat(test)
+
+locate.convert_split_pairbed('split.temp', 'split.bedpe')
+os.remove('split.temp')
+split_bedpe = pybedtools.BedTool('split.bedpe')
 
 # bedtools pairtobed to find TE intersections xor and neither for disc and split reads, sort
-te_intersect_disc = disc.pair_to_bed(te, f=0.1, type='xor')
-no_intersect_disc = disc.pair_to_bed(te, f=0.1, type='neither')
+te_intersect_disc = disc.pair_to_bed(te, f=0.1, type='xor').sort().moveto('intersect.temp')
+no_intersect_disc = disc.pair_to_bed(te, f=0.1, type='neither').sort().moveto('no_intersect.temp')
+split_no_intersect = split_bedpe.pair_to_bed(te, f=0.1, type='neither').sort().moveto('split_no_intersect.temp')
 
-# reorder columns so that TE read is in second position (reorder.py does this, need to update code)
-locate.reorder(te_intersect_disc)
-locate.reorder(no_intersect_disc)
+# reorder columns so that TE read is in second position
+# might be better to create a dictionary and go straight to second part
+locate.reorder('intersect.temp', 'reorder_intersect.bed')
+locate.reorder('no_intersect.temp', 'reorder_no_intersect.bed')
+locate.reorder('split_no_intersect.temp', 'reorder_split_no_intersect.bed')
 
-# sort by TE name and split into file / objects for each TE
+os.remove('intersect.temp')
+os.remove('no_intersect.temp')
+os.remove('split_no_intersect.temp')
 
-# bedtools sort and merge
+"""
+split intersections file by TE name into different files
+define function that reads file, creates bedtool object for each TE name
+and does bedtools merge and sort on each bedtool object,
+then cats all objects and destroys intermediates,
+save final object to file
 
+Can do this with BedTool.filter() and create a function that will filter out lines
+where TE == input name, do for list of unique names. Can go straight from filtered list to
+merge, then filter where both strands, then append to final BedTool object.
+
+Can also do parellel_apply
+"""
 
 # filter out where there are reads on different strands
 
