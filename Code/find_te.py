a98c0b8efee52dad69e510997cd08366786e95d4
timoast <timstuart90@gmail.com>
2014-11-19 14:21:41 +0800

    Reorganised code

--- Code/find_te.py
+++ Code/find_te.py
@@ -3,64 +3,52 @@ import locate
 import os
 
 
-# path to discordant reads bam file
-disc_mapped = locate.checkArgs('-d', '--disc')
-
-# path to mapped bam file
+# mapped bam file
 all_mapped = locate.checkArgs('-c', '--conc')
-
-# path to split read bam file
-split_mapped = locate.checkArgs('-s', '--split')
-
-# path to TE bedfile. Doesn't need to be unzipped
-te_bed = locate.checkArgs('-t', '--te')
-
-# create bedtool objects, convert to bed, sort
 bam = pybedtools.BedTool(all_mapped)
 mn, std = locate.calc_mean(bam.head(20000))  # estimate lib size
 
-disc = pybedtools.BedTool(disc_mapped).bam_to_bed(bedpe=True, mate1=True).sort()
-split = pybedtools.BedTool(split_mapped).bam_to_bed().sort().saveas('split.temp')
-te = pybedtools.BedTool(te_bed).sort()
-
-# remove reads mapped to mito, chlr genomes. May not be needed..
-
-# Convert split bed to bedpe format (already have python script)
-locate.convert_split_pairbed('split.temp', 'split.bedpe')
-os.remove('split.temp')
+# split read data
+split_mapped = locate.checkArgs('-s', '--split')
+split = pybedtools.BedTool(split_mapped).bam_to_bed().sort().saveas('split.bed')
+locate.convert_split_pairbed('split.bed', 'split.bedpe')
 split_bedpe = pybedtools.BedTool('split.bedpe')
+merged_split = split.merge(c='2,3', o='count_distinct,count_distinct')
+breakpoints = locate.filter_split(merged_split)
+
+# discordant reads
+disc_mapped = locate.checkArgs('-d', '--disc')
+disc = pybedtools.BedTool(disc_mapped).bam_to_bed(bedpe=True, mate1=True).sort()
+
+# TE bedfile
+te_bed = locate.checkArgs('-t', '--te')
+te = pybedtools.BedTool(te_bed).sort()
 
 # bedtools pairtobed to find TE intersections xor and neither for disc and split reads
 te_intersect_disc = disc.pair_to_bed(te, f=0.1, type='xor').moveto('intersect.temp')
-no_intersect_disc = disc.pair_to_bed(te, f=0.1, type='neither').moveto('no_intersect.temp')
-split_no_intersect = split_bedpe.pair_to_bed(te, f=0.1, type='neither').moveto('split_no_intersect.temp')
+no_intersect_disc = disc.pair_to_bed(te, f=0.1, type='neither')
+no_intersect_split = split_bedpe.pair_to_bed(te, f=0.1, type='neither')
+no_intersect = no_intersect_disc.cat(no_intersect_split, postmerge=False).sort()
 
 # reorder columns so that TE read is in second position
 locate.reorder('intersect.temp', 'reorder_intersect.bed')
-locate.reorder('no_intersect.temp', 'reorder_no_intersect.bed')
-locate.reorder('split_no_intersect.temp', 'reorder_split_no_intersect.bed')
 
 # remove intermediate files
 os.remove('intersect.temp')
-os.remove('no_intersect.temp')
-os.remove('split_no_intersect.temp')
 
 # Now create bedtool objects again, reordered
 te_intersect_disc_ordered = pybedtools.BedTool('reorder_intersect.bed').sort()
-no_intersect_ordered = pybedtools.BedTool('reorder_no_intersect.bed')
-split_no_intersect_ordered = pybedtools.BedTool('reorder_split_no_intersect')
-no_intersect_ordered = no_intersect_ordered.cat(split_no_intersect_ordered, postmerge=False).sort()
+
 
 # merge intersection coordinates where TE name is the same
 merged_intersections = locate.merge_TE_coords(te_intersect_disc_ordered, 9)
 
 # filter out where there are reads on different strands
-strands = merged_intersections.filter(lambda b: len(b[x]) < 2)  # set x to column containing collapsed strand info
-merged_intersections = strands
+strands = merged_intersections.filter(lambda b: len(b[x]) < 2).saveas('collapsed_intersections.temp')  # set x to column containing collapsed strand info
 
 # python annotate_ins.py
-
-# bedtools merge split reads and count unique coords to find breakpoints
+# need way to get accession name
+locate.annotate_insertions('collapsed_intersections.temp', 'insertions.bed', name, mn, std)
 
 # filter out where there is a breakpoint - separate_breakpoints.py
 
