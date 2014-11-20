496dff4083bd9af668bd3d52a33ed1cf3340d20d
timoast <timstuart90@gmail.com>
2014-11-20 08:42:59 +0800

    Added feedback during processing

--- Code/find_te.py
+++ Code/find_te.py
@@ -13,22 +13,29 @@ bam = pybedtools.BedTool(all_mapped)
 mn, std = locate.calc_mean(bam.head(20000))  # estimate lib size
 
 # split read data
+print 'Processing split reads'
 split_mapped = locate.checkArgs('-s', '--split')
 split = pybedtools.BedTool(split_mapped).bam_to_bed().sort().saveas('split.bed')
 locate.convert_split_pairbed('split.bed', 'split.bedpe')
 split_bedpe = pybedtools.BedTool('split.bedpe')
 merged_split = split.merge(c='2,3', o='count_distinct,count_distinct')
-filtered_split = locate.filter_split(merged_split)
+
+# need to filter out mito, chlr genes before this step
+locate.filter_split(merged_split)  # error loading bedtool file midway through processing
+filtered_split = pybedtools.BedTool('filtered_split.temp')
 
 # discordant reads
+print 'Processing discordant reads'
 disc_mapped = locate.checkArgs('-d', '--disc')
 disc = pybedtools.BedTool(disc_mapped).bam_to_bed(bedpe=True, mate1=True).sort()
 
 # TE bedfile
+print 'Processing TE annotation'
 te_bed = locate.checkArgs('-t', '--te')
 te = pybedtools.BedTool(te_bed).sort()
 
 # bedtools pairtobed to find TE intersections xor and neither for disc and split reads
+print 'Intersecting TE coordinates with reads'
 te_intersect_disc = disc.pair_to_bed(te, f=0.1, type='xor').moveto('intersect.temp')
 no_intersect_disc = disc.pair_to_bed(te, f=0.1, type='neither')
 no_intersect_split = split_bedpe.pair_to_bed(te, f=0.1, type='neither')
@@ -41,12 +48,14 @@ locate.reorder('intersect.temp', 'reorder_intersect.bed')
 te_intersect_disc_ordered = pybedtools.BedTool('reorder_intersect.bed').sort()
 
 # merge intersection coordinates where TE name is the same
+print 'Merging TE intersections'
 merged_intersections = locate.merge_TE_coords(te_intersect_disc_ordered, 9)
 
 # filter out where there are reads on different strands
 strands = merged_intersections.filter(lambda b: len(b[-1]) < 2).saveas('collapsed_intersections.temp')  # check column is correct
 
 # Find where there are breakpoints at insertion site
+print 'Annotating insertions'
 locate.annotate_insertions('collapsed_intersections.temp', 'insertions.temp', name, mn, std)
 pybedtools.BedTool('insertions.temp').intersect(filtered_split, c=True).moveto('insertions_split_reads.temp')
 locate.splitfile('insertions_split_reads.temp')  # separate breakpoints. make single and double breakpoint files
