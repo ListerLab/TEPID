33d8786e2eec67c002a30985d94830aadc1e29f5
timoast <timstuart90@gmail.com>
2015-03-03 15:52:13 +0800

    Updated simulation scripts
    Works with new version of RSVSim
    allowing specification of cut-paste
    or copy-paste transpositions

--- Testing/RSVSim_commands.R
+++ Testing/RSVSim_commands.R
@@ -41,86 +41,76 @@ brachy_deletions_r3 <- with(del_brachy_r3, GRanges(chr, IRanges(start, stop)))
 brachy_deletions_r4 <- with(del_brachy_r4, GRanges(chr, IRanges(start, stop)))
 brachy_deletions_r5 <- with(del_brachy_r5, GRanges(chr, IRanges(start, stop)))
 
-ath_insertions_r1 <- with(ins_tair_r1, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start))
-ath_insertions_r2 <- with(ins_tair_r2, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start))
-ath_insertions_r3 <- with(ins_tair_r3, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start))
-ath_insertions_r4 <- with(ins_tair_r4, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start))
-ath_insertions_r5 <- with(ins_tair_r5, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start))
+ath_insertions_r1 <- with(ins_tair_r1, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start, copied=is_copied))
+ath_insertions_r2 <- with(ins_tair_r2, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start, copied=is_copied))
+ath_insertions_r3 <- with(ins_tair_r3, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start, copied=is_copied))
+ath_insertions_r4 <- with(ins_tair_r4, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start, copied=is_copied))
+ath_insertions_r5 <- with(ins_tair_r5, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start, copied=is_copied))
 
-brachy_insertions_r1 <- with(ins_brachy_r1, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start))
-brachy_insertions_r2 <- with(ins_brachy_r2, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start))
-brachy_insertions_r3 <- with(ins_brachy_r3, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start))
-brachy_insertions_r4 <- with(ins_brachy_r4, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start))
-brachy_insertions_r5 <- with(ins_brachy_r5, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start))
+brachy_insertions_r1 <- with(ins_brachy_r1, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start, copied=is_copied))
+brachy_insertions_r2 <- with(ins_brachy_r2, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start, copied=is_copied))
+brachy_insertions_r3 <- with(ins_brachy_r3, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start, copied=is_copied))
+brachy_insertions_r4 <- with(ins_brachy_r4, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start, copied=is_copied))
+brachy_insertions_r5 <- with(ins_brachy_r5, GRanges(chr, IRanges(start, stop), chrB=dest_chr, startB=dest_start, copied=is_copied))
 
 # simulate variation
 simulateSV(output="./ath/r1",
            genome=tair10,
            regionsDels=ath_deletions_r1,
            regionsIns=ath_insertions_r1,
-           random=FALSE,
-           percCopiedIns=0.5)
+           random=FALSE)
 
 simulateSV(output="./ath/r2",
            genome=tair10,
            regionsDels=ath_deletions_r2,
            regionsIns=ath_insertions_r2,
-           random=FALSE,
-           percCopiedIns=0.5)
+           random=FALSE)
 
 simulateSV(output="./ath/r3",
            genome=tair10,
            regionsDels=ath_deletions_r3,
            regionsIns=ath_insertions_r3,
-           random=FALSE,
-           percCopiedIns=0.5)
+           random=FALSE)
 
 simulateSV(output="./ath/r4",
            genome=tair10,
            regionsDels=ath_deletions_r4,
            regionsIns=ath_insertions_r4,
-           random=FALSE,
-           percCopiedIns=0.5)
+           random=FALSE)
 
 simulateSV(output="./ath/r5",
            genome=tair10,
            regionsDels=ath_deletions_r5,
            regionsIns=ath_insertions_r5,
-           random=FALSE,
-           percCopiedIns=0.5)
+           random=FALSE)
 
 
 simulateSV(output="./brachy/r1",
            genome=brachy,
            regionsDels=brachy_deletions_r1,
            regionsIns=brachy_insertions_r1,
-           random=FALSE,
-           percCopiedIns=0.5)
+           random=FALSE)
 
 simulateSV(output="./brachy/r2",
            genome=brachy,
            regionsDels=brachy_deletions_r2,
            regionsIns=brachy_insertions_r2,
-           random=FALSE,
-           percCopiedIns=0.5)
+           random=FALSE)
 
 simulateSV(output="./brachy/r3",
            genome=brachy,
            regionsDels=brachy_deletions_r3,
            regionsIns=brachy_insertions_r3,
-           random=FALSE,
-           percCopiedIns=0.5)
+           random=FALSE)
 
 simulateSV(output="./brachy/r4",
            genome=brachy,
            regionsDels=brachy_deletions_r4,
            regionsIns=brachy_insertions_r4,
-           random=FALSE,
-           percCopiedIns=0.5)
+           random=FALSE)
 
 simulateSV(output="./brachy/r5",
            genome=brachy,
            regionsDels=brachy_deletions_r5,
            regionsIns=brachy_insertions_r5,
-           random=FALSE,
-           percCopiedIns=0.5)
+           random=FALSE)
--- Testing/generate_sim_coords.py
+++ Testing/generate_sim_coords.py
@@ -79,23 +79,30 @@ class sim_data():
                                                                stp=info['stop'],
                                                                TE=selection))
 
-    def create_insertion_coords(self, outfile, n=100):
+    def create_insertion_coords(self, outfile, percCopied=0.5, n=100):
         """
         randomly select n transposon coordinates and randomly generate insertion points
         save to outfile
         """
-        with open(outfile, 'w+') as ins:
-            ins.write("chr\tstart\tstop\tdest_chr\tdest_start\n")
+        n_copied = n*percCopied
+        with open(outfile, 'a+') as ins:
+            ins.write("chr\tstart\tstop\tdest_chr\tdest_start\tis_copied\n")
             for x in xrange(n):
                 selection = random.choice(self.transposons.keys())
                 info = self.transposons[selection]
                 dest_chrom = random.choice(self.chromosomes.keys())
                 dest_start = random.randint(0, self.chromosomes[dest_chrom])
-                ins.write("{ch}\t{sta}\t{stp}\t{dest_ch}\t{dest_sta}\n".format(ch=info['chrom'],
-                                                                               sta=info['start'],
-                                                                               stp=info['stop'],
-                                                                               dest_ch=dest_chrom,
-                                                                               dest_sta=dest_start))
+                if n_copied > 0:
+                    copied = 'TRUE'
+                    n_copied -= 1
+                else:
+                    copied = 'FALSE'
+                ins.write("{ch}\t{sta}\t{stp}\t{dest_ch}\t{dest_sta}\t{copied}\n".format(ch=info['chrom'],
+                                                                                         sta=info['start'],
+                                                                                         stp=info['stop'],
+                                                                                         dest_ch=dest_chrom,
+                                                                                         dest_sta=dest_start,
+                                                                                         copied=copied))
 
 if __name__ == "__main__":
     genome = checkArgs('-g', '--genome')
