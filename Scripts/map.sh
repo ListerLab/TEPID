3baa1189118830ee82e26a3ab884322ae41a7721
timoast <timstuart90@gmail.com>
2015-03-26 11:16:35 +0800

    Moved code into function

--- Scripts/map.sh
+++ Scripts/map.sh
@@ -29,47 +29,8 @@ shift $((OPTIND - 1))
 samtools --version
 samblaster --version
 
-if [ "$recursive" == true ]; then
-  for directory in ./*; do
-      if [ -d "$directory" ]; then
-          cd $directory
+map () {
 
-          for myfile in $(ls -d *_1.fastq);do
-
-            fname=(${myfile//_1.fastq/ })
-
-            echo "Mapping ${fname}"
-            bowtie2 --local --dovetail -p$proc --fr -q -R5 -N1 -x $index -X $size\
-             -1 "${fname}_1.fastq" -2 "${fname}_2.fastq" \
-            | samblaster -e -u "${fname}.umap.fastq" \
-            | samtools view -b - > "${fname}.bam"
-
-            yaha -t $proc -x $yhindex -q "${fname}.umap.fastq" -L 11 -H 2000 -M 15 -osh stdout \
-            | samblaster -s "${fname}.split.sam" > /dev/null
-
-            echo "Converting to bam"
-            samtools view -Sb@ $proc "${fname}.split.sam" > "${fname}.split.bam"
-            rm "${fname}.split.sam"
-
-            echo "Sorting alignment"
-            samtools sort -@ $proc "${fname}.bam" "${fname}_sorted"
-            rm "${fname}.bam"
-
-            echo "Indexing alignment"
-            samtools index "${fname}_sorted.bam"
-
-            if [ "$zip" == true ]; then
-              echo "Zipping fastq files"
-              gzip *.fastq
-            fi
-
-          done
-
-          cd ..
-      fi
-  done
-
-else
   for myfile in $(ls -d *_1.fastq);do
 
     fname=(${myfile//_1.fastq/ })
@@ -100,4 +61,17 @@ else
     fi
 
 done
+}
+
+if [ "$recursive" == true ]; then
+  for directory in ./*; do
+      if [ -d "$directory" ]; then
+          cd $directory
+          map
+          cd ..
+      fi
+  done
+
+else
+  map
 fi
