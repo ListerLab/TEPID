7c2276dca0c5f2cded586b78f175174a6673b5da
timoast <timstuart90@gmail.com>
2015-03-26 10:52:22 +0800

    Moved mapping code into one script

--- Scripts/map.sh
+++ Scripts/map.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-index=  proc=  yhindex=  size=  recursive=  zip=  repo=  
+index=  proc=  yhindex=  size=  recursive=  zip=  
 
-while getopts x:p:y:s:rzc: opt; do
+while getopts x:p:y:s:rz opt; do
   case $opt in
   x)
       index=$OPTARG
@@ -22,12 +22,12 @@ while getopts x:p:y:s:rzc: opt; do
   z)
       zip=true
       ;;
-  c)
-      repo=$OPTARG
-      ;;
   esac
 done
 shift $((OPTIND - 1))
+
+samtools --version
+samblaster --version
 
 if [ "$zip" == true ]; then
   zip=$zip
@@ -39,11 +39,71 @@ if [ "$recursive" == true ]; then
   for directory in ./*; do
       if [ -d "$directory" ]; then
           cd $directory
-          sh $repo/Scripts/process_single.sh -p $proc -x $index -y $yhindex -s $size -z $zip
+
+          for myfile in $(ls -d *_1.fastq);do
+
+            fname=(${myfile//_1.fastq/ })
+
+            echo "Mapping ${fname}"
+            bowtie2 --local --dovetail -p$proc --fr -q -R5 -N1 -x $index -X $size\
+             -1 "${fname}_1.fastq" -2 "${fname}_2.fastq" \
+            | samblaster -e -u "${fname}.umap.fastq" \
+            | samtools view -b - > "${fname}.bam"
+
+            yaha -t $proc -x $yhindex -q "${fname}.umap.fastq" -L 11 -H 2000 -M 15 -osh stdout \
+            | samblaster -s "${fname}.split.sam" > /dev/null
+
+            echo "Converting to bam"
+            samtools view -Sb@ $proc "${fname}.split.sam" > "${fname}.split.bam"
+            rm "${fname}.split.sam"
+
+            echo "Sorting alignment"
+            samtools sort -@ $proc "${fname}.bam" "${fname}_sorted"
+            rm "${fname}.bam"
+
+            echo "Indexing alignment"
+            samtools index "${fname}_sorted.bam"
+
+            if [ "$zip" == true ]; then
+              echo "Zipping fastq files"
+              gzip *.fastq
+            fi
+
+          done
+
           cd ..
       fi
   done
 
 else
-  sh $repo/Scripts/process_single.sh -p $proc -x $index -y $yhindex -s $size -z $zip
+  for myfile in $(ls -d *_1.fastq);do
+
+    fname=(${myfile//_1.fastq/ })
+
+    echo "Mapping ${fname}"
+    bowtie2 --local --dovetail -p$proc --fr -q -R5 -N1 -x $index -X $size\
+     -1 "${fname}_1.fastq" -2 "${fname}_2.fastq" \
+    | samblaster -e -u "${fname}.umap.fastq" \
+    | samtools view -b - > "${fname}.bam"
+
+    yaha -t $proc -x $yhindex -q "${fname}.umap.fastq" -L 11 -H 2000 -M 15 -osh stdout \
+    | samblaster -s "${fname}.split.sam" > /dev/null
+
+    echo "Converting to bam"
+    samtools view -Sb@ $proc "${fname}.split.sam" > "${fname}.split.bam"
+    rm "${fname}.split.sam"
+
+    echo "Sorting alignment"
+    samtools sort -@ $proc "${fname}.bam" "${fname}_sorted"
+    rm "${fname}.bam"
+
+    echo "Indexing alignment"
+    samtools index "${fname}_sorted.bam"
+
+    if [ "$zip" == true ]; then
+      echo "Zipping fastq files"
+      gzip *.fastq
+    fi
+
+done
 fi
