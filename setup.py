253d47c72a2b91e2c1f29bbade94d144e8deef90
timoast <timstuart90@gmail.com>
2015-04-08 15:05:00 +0800

    Change version info

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup
+import os
+
+version_file = open(os.path.join(os.path.dirname(__file__), 'VERSION'))
+version = version_file.read().strip()
 
 setup(
     name = 'locaTE',
-    version = '1.0',
+    version = version,
     description = 'Discover transposable element insertion sites',
     author = 'Tim Stuart',
     author_email = 'timstuart90@gmail.com',
