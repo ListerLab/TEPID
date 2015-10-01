53c97e91674496b99f05cc7f0887cc992f81facc
timoast <timstuart90@gmail.com>
2015-10-01 16:44:27 +0800

    Add git versioning

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from distutils.core import setup
 import os
+import subprocess
 
-version_file = open(os.path.join(os.path.dirname(__file__), 'VERSION'))
-version = version_file.read().strip()
+version_file = os.path.join(os.path.dirname(__file__), 'VERSION')
+
+# update version file if in git repo
+try:
+    version_git = subprocess.check_output(["git", "describe"]).rstrip()
+except:
+    version_git = open(version_file).read().strip()
+with open(version_file, 'w') as fh:
+    fh.write(version_git)
+
 
 setup(
     name = 'TEpy',
-    version = version,
+    version = version_git,
     description = 'Discover transposable element insertion sites',
     author = 'Tim Stuart',
     author_email = 'timstuart90@gmail.com',
