6d9737449527c0fd86748043be73c8ea74a3e728
timoast <timstuart90@gmail.com>
2015-10-02 14:35:53 +0800

    Use versioneer

--- setup.py
+++ setup.py
@@ -1,28 +1,15 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-import os
-import subprocess
-
-version_file = os.path.join(os.path.dirname(__file__), 'VERSION')
-
-# update version file if in git repo
-try:
-    version_git = subprocess.check_output(["git", "describe"]).rstrip()
-except:
-    version_git = open(version_file).read().strip()
-with open(version_file, 'w') as fh:
-    fh.write(version_git)
+#!/usr/bin/env python
+from setuptools import setup
+import versioneer
 
 
 setup(
     name = 'TEpy',
-    version = version_git,
-    description = 'Discover transposable element insertion sites',
+    version = versioneer.get_version(),
+    description="TEpy: Discover transposable element insertion sites",
     author = 'Tim Stuart',
     author_email = 'timstuart90@gmail.com',
     url = 'https://github.com/timoast/TEpy',
     scripts = ["Scripts/tepy", "Scripts/tepy_map.sh"],
-    packages = [''],
+    packages = ['tepy'],
 )
