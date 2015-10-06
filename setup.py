7be0a8b0e0e773db1196a2106cfa9fc7a1241214
timoast <timstuart90@gmail.com>
2015-10-06 10:51:13 +0800

    Check samtools and bedtools versions

--- setup.py
+++ setup.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python
 from setuptools import setup
 import versioneer
+import subprocess
+
+
+def samtools():
+    major, minor = subprocess.check_output(['samtools', '--version']).split()[1].split('.')
+    if int(major) >= 1 and int(minor) >= 1:
+        return True
+    return False
+
+
+def bedtools():
+    p = subprocess.check_output(['bedtools', '--version'])
+    major, minor, micro = p.split()[1].split('.')
+    major = major[1]
+    if int(major) >= 2 and int(minor) >= 24 and int(micro) >= 0:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    if not samtools():
+        raise Exception("TEpy requires samtools v1.1 or greater")
+    if not bedtools():
+        raise Exception("TEpy requires bedtools v2.24.0 or greater")
 
 
 setup(
